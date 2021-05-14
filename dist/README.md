4a87a24fc002b24544f371c03e3f6344c5ca450b
jtstockard <jtstockard92@gmail.com>
2021-05-14 12:03:54 -0500

    prompt functions and README temp added

---
+++ dist/README.md
@@ -0,0 +1,50 @@
+<h1 align="center">README Generator</h1>
+  
+![badge](https://img.shields.io/badge/license-MIT-brightgreen)<br />
+
+## Description
+
+A node.js application that creates a generated README.md file to the dist directory
+
+## Table of Contents
+
+- [Description](#description)
+- [Table of Contents](#table-of-contents)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+npm install inquirer is required
+
+## Usage
+
+UT Coding Bootcamp class assignment
+
+## License
+
+![badge](https://img.shields.io/badge/license-MIT-brightgreen)
+<br />
+This application is covered by the MIT license.
+
+## Contributing
+
+Joel Stockard
+
+## Tests
+
+No tests are included
+
+## Questions
+
+Email me or reach out on GitHub<br />
+<br />
+Find me on GitHub: [jpd61](https://github.com/jtstockard)<br />
+<br />
+Email me with any questions: jtstockard@gmail.com<br /><br />
+
+\_This README was generated with ❤️ by [README-generator](https://github.com/jtstockard/README_Generator)
--- index.js
+++ index.js
@@ -1,13 +1,81 @@
-// TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
+//Dependencies and variables that are provided by npm install
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const generateMarkdown = require("./utils/generateMarkdown");
+const writeFileAsync = util.promisify(fs.writeFile);
+// Questions asked to the user
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the title for your project?",
+      name: "projectTitle",
+    },
+    {
+      type: "input",
+      message: "Write a description of your project:",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "Describe the installation process if any:",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "Please provide information usage for your project:",
+      name: "usage",
+    },
+    {
+      type: "list",
+      message: "What license was used for this README?",
+      name: "license",
+      choices: [
+        "None",
+        "Apache License 2.0",
+        "GNU General Public License v3.0",
+        "MIT License",
+        "BSD 2-Clause Simplified License",
+        "BSD 3-Clause New or Revised License",
+        "Boost Software License 1.0",
+      ],
+    },
+    {
+      type: "input",
+      message: "Please advise any contributions you have made:",
+      name: "contributions",
+    },
+    {
+      type: "input",
+      message: "Please advise your test opporation performed for your project:",
+      name: "tests",
+    },
+    {
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "username",
+    },
+    {
+      type: "input",
+      message: "What is your email address?",
+      name: "email",
+    },
+  ]);
+}
+//Async function uses util.promisify
+async function init() {
+  try {
+    // Ask user questions and generate responses
+    const answers = await promptUser();
+    const generateContent = generateMarkdown(answers);
+    // Write new README.md to dist directory
+    await writeFileAsync("./dist/README.md", generateContent);
+    console.log("✔️  Successfully wrote to README.md");
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 // Function call to initialize app
 init();
