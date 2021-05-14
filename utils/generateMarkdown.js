334d10bf767072872b58479a85662610251f68dc
jtstockard <jtstockard92@gmail.com>
2021-05-14 14:10:33 -0500

    README generator with prompts together

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,8 +11,63 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
+  return `
+  <h1 align="center">${answers.projectTitle} 👋</h1>
+    
+  ![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)<br />
+
+  ## Description
+
+  ${answers.description}
+
+  ## Table of Contents
+
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+
+  ${answers.installation}
+
+  ## Usage
+
+  ${answers.usage}
+
+  ## License
+
+  ![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
+  <br />
+
+  This application is covered by the ${answers.license} license. 
+  
+  ## Contributing
+
+  ${answers.contributing}
+
+  ## Tests
+
+  ${answers.tests}
+
+  ## Questions
+
+  ✋ ${answers.questions}<br />
+  <br />
+
+   Find me on GitHub: [${answers.username}](https://github.com/${answers.username})<br />
+  <br />
+
+  Email me with any questions: ${answers.email}<br /><br />
+
+  This README was generated with ❤️ by [README-generator](https://github.com/jtstockard/README-generator)
+      ;
+    }
+    
 
 `;
 }
